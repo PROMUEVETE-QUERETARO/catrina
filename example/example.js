4c0103e2f2ca624bb15e411bc2c265be7abed8b5
jonhteper <jonhteper@protonmail.com>
2022-01-08 16:16:34 -0600

    feat: add collapse boxes for code example

--- example/example.js
+++ example/example.js
@@ -1,1 +1,11 @@
+import * as catrina from "../widgets/catrina.js";
+
+const SHOW_LABEL = "Show code";
+let id = document.getElementById.bind(document);
+let datas = [id('pre-radio'),id('pre-checkbox'), id('pre-file')];
+catrina.CollapseBox(id("codebox15"), id('pre-checkbox').innerHTML, SHOW_LABEL, 'show-radio15');
+catrina.CollapseBox(id("codebox16"), id('pre-radio').innerHTML, SHOW_LABEL, 'show-radio16');
+catrina.CollapseBox(id("codebox17"), id('pre-file').innerHTML, SHOW_LABEL, 'show-radio17');
+catrina.collapseBoxes_run();
+datas.map(d => d.remove());
 console.log("Catrina example");
