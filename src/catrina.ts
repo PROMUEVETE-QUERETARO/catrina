d638b2bfbd0926519f57c7ffee2c3dbeffbf8443
jonhteper <jonhteper@protonmail.com>
2022-01-12 19:11:41 -0600

    feat: export new functions globally

--- src/catrina.ts
+++ src/catrina.ts
@@ -1,2 +1,3 @@
-export {GetTemplate} from "./core.js"
+export {Button, PrimaryButton,DangerousButton} from "./core.js"
 export {CollapseBox, collapseBoxes_run} from "./collapse-box/collapse-box.js"
+export {Alert} from "./alerts/alert.js"
