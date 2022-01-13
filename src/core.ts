aa16b13100787a911e00b5a595c3bc3f23676182
jonhteper <jonhteper@protonmail.com>
2022-01-12 19:10:38 -0600

    feat: generate buttons functions

--- src/core.ts
+++ src/core.ts
@@ -1,7 +1,21 @@
 export const HIDDEN_CLASS = "_hidden";
 
-export async function GetTemplate(templateSrc: string): Promise<string> {
-    return fetch(templateSrc).then(d => d.text())
-
+export function Button(id: string, text: string): HTMLButtonElement {
+    return __button(id, text, 'c__button')
 }
 
+export function PrimaryButton(id: string, text: string): HTMLButtonElement {
+    return __button(id, text, 'c__button c__button--primary')
+}
+
+export function DangerousButton(id: string, text: string): HTMLButtonElement {
+    return __button(id, text, 'c__button c__button--dangerous')
+}
+
+function __button(id:string, text:string, classList: string): HTMLButtonElement {
+    let button = document.createElement('button');
+    button.id = id;
+    button.innerText = text;
+    button.className = classList;
+    return button;
+}
